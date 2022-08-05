193936066038fa194bdf3cd786391fe5aa63bbea
Victor Alecrim <94874313+Victoralecrim@users.noreply.github.com>
2022-08-05 11:01:01 -0300

    Add files via upload

--- js/loginform.js
+++ js/loginform.js
@@ -1,6 +1,8 @@
-function showPassword() {
+function showPasswordAndValidateForm() {
     const eye = document.getElementById('eye');
     const eyeSlash = document.getElementById('eye-slash');
+    //Pegando o id do input de email e senha
+    const inputemail = document.getElementById('email');
     const fieldPassword = document.getElementById('field-password');
 
     if (eye.style.display === 'none') {
@@ -12,15 +14,6 @@ function showPassword() {
         eyeSlash.style.display = 'block';
         fieldPassword.type = 'password';
     }
-   
-}
-
-
-function ValidateForm() {
-    //Pegando o id do input de email 
-    const inputemail = document.getElementById('email');
-    //Pegando o id do input Password
-    const inputpassword = document.getElementById('field-password');
 
     //Verificando o campo de email
 
@@ -28,22 +21,23 @@ function ValidateForm() {
         alert("Campo de email vazio !");
         //Deixa o input com foco ao clicar
         email.focus();
-        //retorna a função e ignora as outras linhas
-        return;
     }
 
     //Verificando o campo de senha
 
-    if(inputpassword.value == ""){
+    if (fieldPassword.value == "") {
         alert("Campo de senha vazio, por favor informe uma senha !");
-        password.focus();
-        return;
+        fieldPassword.focus();
     }
 
+    else {
+        document.getElementById('btn-login').addEventListener('click', function (e) {
+            e.preventDefault();
+            alert('Campo de email e senha vazios');
+        });
+    }
 
 }
 
-document.getElementById('btn-login').addEventListener('click', function (e) {
-    e.preventDefault();
-    alert('Logado!');
-});
+
+
